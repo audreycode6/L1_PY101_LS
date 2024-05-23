345696641d59d000f11d670b1586ff7cb33f146d
Audrey Theriault-Allaire <audrey2699@gmail.com>
2024-05-23 10:36:47 -0700

    wip

--- easy3_small_problems/daily_double.py
+++ easy3_small_problems/daily_double.py
@@ -12,28 +12,49 @@
 # TODO:
     # string input
     # define function takes in string
-    # modify if duplicates : next to on another
+    # loop through chars and look for duplicates
+    # store 1st char == next element if so pop if not continue and next char == 1st char?
+    # modify if duplicates : next to one another
     # return string with no duplicates
-string = input('Enter a word or phrase: ')
-def crunch(string):
-    # print(set(string)) # does remove dups but scrambles string order ..
 
-    set_string = set(string) # removes dupes
-    string_length = len(string)
-    if len(set_string) == string_length: # no dupes
-        return string
-    else: # has dupes
-        # TODO: figure out how to alter string if has dupes
+# string = input('Enter a word or phrase: ')
+def remove_duplicate_chars(string):
+    if len(string) > 1: # if string longer than 1 char
+        no_dupe_list = []
+        length = len(string)
+        next_index = 1
+
         for char in string:
-            if string.count(char) > 1:
-                dupe_char = char #hello --> l
+            if char == string[next_index]:
+                print(f'{char} is a dupe')
+            else:
+                no_dupe_list.append(char)
+                # print(no_dupe_string)
+            next_index += 1
+            if next_index == length:
+                last_char = string[-1]
+                no_dupe_list.append(last_char)
+                break # look up what break does, i think it just cancels whole function, does it return(?)
+# TODO find out how to make no_dope_list to str 
+        return(no_dupe_list)
+    else: # if string less than 2 chars just return string
+        return(string)
+
+# remove_duplicate_chars(string)
+# print(remove_duplicate_chars(string))
+
+
+
+# str.replace(old, new[, count])Return a copy of the string with all occurrences of substring 
+    # old replaced by new. If the optional argument count is given,
+    # only the first count occurrences are replaced.
                 
   
-print(crunch(string))
+
 # These examples should all print True
-# print(crunch('ddaaiillyy ddoouubbllee') == 'daily double')
-# print(crunch('4444abcabccba') == '4abcabcba')
-# print(crunch('ggggggggggggggg') == 'g')
-# print(crunch('abc') == 'abc')
-# print(crunch('a') == 'a')
-# print(crunch('') == '')
+print(remove_duplicate_chars('ddaaiillyy ddoouubbllee') == 'daily double')
+print(remove_duplicate_chars('4444abcabccba') == '4abcabcba')
+print(remove_duplicate_chars('ggggggggggggggg') == 'g')
+print(remove_duplicate_chars('abc') == 'abc')
+print(remove_duplicate_chars('a') == 'a')
+print(remove_duplicate_chars('') == '')
